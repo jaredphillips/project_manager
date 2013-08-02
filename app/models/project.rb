07435adcd96bbd81f9f9bb5def2e881ad06e8e17
Jared Phillips <jaredtphillips@me.com>
2013-08-02 18:18:25 -0400

    Transistion to new bootstrap almost complete

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
 	has_many :tasks, dependent: :destroy
 	has_many :users
 
+  accepts_nested_attributes_for :tasks, allow_destroy: true, reject_if: :all_blank
 	validates :title, presence: true
 	validates :due_date, presence: true
 end
