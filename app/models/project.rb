572794ed44d62a2e942645d348dad13ac976aac7
Jared Phillips <jaredtphillips@me.com>
2013-08-06 15:46:14 -0400

    Datepicker plug in added for Projects/Tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,11 @@ class Project < ActiveRecord::Base
   accepts_nested_attributes_for :tasks, allow_destroy: true, reject_if: :all_blank
 	validates :title, presence: true
 	validates :due_date, presence: true
+  validate :in_the_future
+
+	def in_the_future 
+		if self.due_date < DateTime.now
+			errors.add(:due_date, "Due dates should be set for the future.")
+		end
+	end
 end
